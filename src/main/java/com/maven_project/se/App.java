2f252f0dfb6921442d435d3e215ddac693094ec1
Mohammed Armaan Uddin <139455329+Armaan016@users.noreply.github.com>
2024-12-05 18:55:43 +0530

    Update App.java

--- src/main/java/com/maven_project/se/App.java
+++ src/main/java/com/maven_project/se/App.java
@@ -5,6 +5,7 @@ package com.maven_project.se;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Armaan!");
+        System.out.println("This is Sample Maven Java Application.")
     }
 }
